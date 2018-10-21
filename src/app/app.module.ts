970cc1b88c48844aab2460e0f8b15a366a1ec65a
Monika Radosiewicz <monika.radosiewicz@gmail.com>
2018-10-21 10:52:29 +0200

    create basic files structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsListComponent } from './products-list/products-list.component';
+import { ProductItemComponent } from './products-list/product-item/product-item.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsListComponent,
+    ProductItemComponent,
+    ShoppingCartComponent
   ],
   imports: [
     BrowserModule
